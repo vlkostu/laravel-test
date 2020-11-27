7c14d75ce3a6690dbc841998b41f28a2eb5b42f9
vlkostu <site.manager.vlad95715@gmail.com>
2020-11-27 03:25:02 +0200

    update seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        # Create episodes
         $episodes = Episode::factory(30)->create();
+        # Create characters
+        $characters = Character::factory(100)->create();
+        # Count episodes
+        $episodesCount = $episodes->count();
+
+        $episodes->each(function ($episode) use ($characters, $episodesCount) {
+            # Rand characters
+            $characters = $characters->random(rand(5, 15));
+            # Sync characters
+            $episode->characters()->attach($characters);
+            # Sync quotes
+            $characters->each(function ($character) use ($episode, $episodesCount) {
+                # Generate quotes
+                $quotes = Quote::factory(rand(3, 7))->create([
+                    'episode_id' => rand(1, $episodesCount),
+                    'character_id' => $character->id
+                ]);
+                # Sync character and episode
+                $character->quotes()->saveMany($quotes);
+                $episode->quotes()->saveMany($quotes);
+            });
+        });
+
+        /*$episodes = Episode::factory(30)->create();
         $characters = Character::factory(100)->create();
 
         $episodes->each(function ($episode) use ($characters) {
@@ -32,6 +57,6 @@ class DatabaseSeeder extends Seeder
                 'episode_id' => rand(1, $episodesCount),
                 'character_id' => $character->id
             ]);
-        });
+        });*/
     }
 }
